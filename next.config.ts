4925e12a1ff3b8ea9d5757dcfdb371d9c3d7ebbb
Jeffrey <j.alteza@betrnk-junket.com>
2025-05-04 21:11:52 +0800

    setup github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // required if you're using <Image />
+  },
+  basePath: '/REPO_NAME', // ← important!
+  trailingSlash: true,
 };
 
 export default nextConfig;
